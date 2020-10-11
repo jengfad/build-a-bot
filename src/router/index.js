b242d7d23766148185e60ffb3519914263a9afa3
Jennifer Fadriquela <jenniferfadriquela@gmail.com>
2020-10-11 10:00:08 +0800

    stuff

---
+++ src/router/index.js
@@ -0,0 +1,73 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import HomePage from '../home/HomePage.vue';
+import RobotBuilder from '../build/RobotBuilder.vue';
+import PartInfo from '../parts/PartInfo.vue';
+import BrowseParts from '../parts/BrowseParts.vue';
+import RobotArms from '../parts/RobotArms.vue';
+import RobotBases from '../parts/RobotBases.vue';
+import RobotHeads from '../parts/RobotHeads.vue';
+import RobotTorsos from '../parts/RobotTorsos.vue';
+import SidebarStandard from '../sidebars/SidebarStandard.vue';
+import SidebarBuild from '../sidebars/SidebarBuild.vue';
+import ShoppingCart from '../cart/ShoppingCart.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [{
+    path: '/',
+    name: 'Home',
+    components: {
+      default: HomePage,
+      sidebar: SidebarStandard,
+    },
+  }, {
+    path: '/build',
+    name: 'Build',
+    components: {
+      default: RobotBuilder,
+      sidebar: SidebarBuild,
+    },
+  }, {
+    path: '/parts/browse',
+    name: 'BrowseParts',
+    component: BrowseParts,
+    children: [
+      {
+        name: 'BrowseHeads',
+        path: 'heads',
+        component: RobotHeads,
+      },
+      {
+        name: 'BrowseArms',
+        path: 'arms',
+        component: RobotArms,
+      },
+      {
+        name: 'BrowseBases',
+        path: 'bases',
+        component: RobotBases,
+      },
+      {
+        name: 'BrowseTorsos',
+        path: 'torsos',
+        component: RobotTorsos,
+      },
+    ],
+  }, {
+    path: '/parts/:partType/:id',
+    name: 'Parts',
+    component: PartInfo,
+    props: true,
+    beforeEnter(to, from, next) {
+      const isValidId = Number.isInteger(Number(to.params.id));
+      next(isValidId);
+    },
+  }, {
+    path: '/cart',
+    name: 'Cart',
+    component: ShoppingCart,
+  }],
+});
---
+++ src/shared/CollapsibleSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="header">
+      <span v-if="open" @click="toggle()">&#x25B2; Collapse</span>
+      <span v-if="!open" @click="toggle()">&#x25BC; Expand</span>
+    </div>
+    <slot v-if="open"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CollapsibleSection',
+  data() {
+    return {
+      open: true,
+    };
+  },
+  methods: {
+    toggle() {
+      this.open = !this.open;
+    },
+  },
+};
+</script>
+
+<style scoped>
+  .header {
+    background-color: #bbb;
+    padding: 3px;
+    cursor: pointer;
+  }
+</style>
